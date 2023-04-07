408d6eff1c21765666c810d6f81852b5c65b9846
mathfrance <mathfrance@hotmail.com>
2023-04-07 16:47:33 -0300

    AllowAdminCommandTests

---
+++ EntertenimentManager.Tests/CommandTests/AllowAdminCommandTests.cs
@@ -0,0 +1,28 @@
+﻿
+using EntertenimentManager.Domain.Commands.Account;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EntertenimentManager.Tests.CommandTests
+{
+    [TestClass]
+    public class AllowAdminCommandTests
+    {
+        private readonly AllowAdminCommand _validCommand = new("fulano@gmail.com", true);
+        private readonly AllowAdminCommand _invalidCommand = new("notEmail.com", true);
+
+        [TestMethod]
+        public void ShouldReturnInvalidWhenCommandIsInvalid()
+        {
+            _invalidCommand.Validate();
+            Assert.IsFalse(_invalidCommand.IsValid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnValidWhenCommandIsValid()
+        {
+            _validCommand.Validate();
+            Assert.IsTrue(_validCommand.IsValid);
+        }
+    }
+
+}
