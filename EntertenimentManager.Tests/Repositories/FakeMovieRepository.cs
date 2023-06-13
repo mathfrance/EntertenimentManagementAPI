e5dd270da26ad6847e2be8c83e1f9b9123f36600
mathfrance <mathfrance@hotmail.com>
2023-06-13 18:04:23 -0300

    CreateMovieCommand e MovieHandler Tests

---
+++ EntertenimentManager.Tests/Repositories/FakeMovieRepository.cs
@@ -0,0 +1,28 @@
+﻿using EntertenimentManager.Domain.Entities.Categories;
+using EntertenimentManager.Domain.Entities.Itens;
+using EntertenimentManager.Domain.Entities.Lists;
+using EntertenimentManager.Domain.Entities.Users;
+using EntertenimentManager.Domain.Repositories.Contracts;
+
+namespace EntertenimentManager.Tests.Repositories
+{
+    public class FakeMovieRepository : IMovieRepository
+    {
+        public Task CreateAsync(Movie movie)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<PersonalList> GetPersonalListById(int id)
+        {
+            if (id == 0)
+            {
+                return Task.FromResult(new PersonalList("Para Assistir"));
+            }
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            return Task.FromResult<PersonalList>(null);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+
+        }
+    }
+}
